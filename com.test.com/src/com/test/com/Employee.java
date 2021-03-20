97babdc3539f0b306791f3385d87bdc22fd47f73
AnuV2C18467 <AnuV2C18467@V2CP0081.vee2office.com>
2021-03-20 13:18:50 +0530

    Strings

---
+++ com.test.com/src/com/test/com/Employee.java
@@ -0,0 +1,25 @@
+package com.test.com;
+
+public class Employee {
+
+		public static void main(String[] args) {
+			String a1 = "Shivagopal";
+			String a2 = null;
+			String a3 = "asdf";
+			String a4 = "Chandrashekar";
+			String a5 = "Rajkala";
+			if(a1.contains("Shiva")) {
+				System.out.print("Found Shiva in "+ a1 );
+			}
+			else {
+				System.out.print("Shiva doesn't contain in " + a1 );
+			}
+			if(a3.isEmpty()) {
+				System.out.print("String is Empty ");
+			}
+			else {
+				System.out.print('\n'+"String is not empty and it contains: " + a3 );
+			}
+		}
+	}
+
